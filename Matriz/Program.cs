3010ced6a4297bc853803144c2f0bb2a8f9dbdf2
Carlos Arruda <caugustogarruda@gmail.com>
2020-05-20 16:25:52 -0300

    Starting project

---
+++ Matriz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number of row and column of matrix: ");
+            int row = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
+            int cont = 0;
+
+            double[,] mat = new double[row, col];
+
+            for(int i = 0; i < row; i++)
+            {
+                Console.Write("Type matrix number: ");
+                string[] numbers = Console.ReadLine().Split(" ");
+                for(int j = 0; j < col; j++)
+                {
+                    mat[i, j] = int.Parse(numbers[j]);
+                }
+            }
+            Console.WriteLine("Main diagonal!");
+            for(int i = 0; i < row; i++)
+            {
+                Console.WriteLine(mat[i, i] + " ");
+            }
+
+            Console.WriteLine("Total of negative numbers!");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if(mat[i,j] < 0)
+                        cont++;
+                }
+            }
+            Console.Write(cont);
+
+        }
+    }
+}
